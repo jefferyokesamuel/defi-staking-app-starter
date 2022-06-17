6d1d75f5f845fbff9ece8572cc5773e4bace8223
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-17 20:59:15 +0100

    Adding the transfer functionality

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -5,4 +5,30 @@ contract Tether {
     string public symbol = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000; //1 million tokens
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed sender,
+        address indexed receiver,
+        uint amount
+    );
+
+    event Approve (
+        address indexed owner,
+        address indexed spender,
+        uint amount
+    );
+
+    mapping(address => uint256) public balanceOf; 
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _reciever, uint _amount) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _amount, "Your balance is smaller than the amount");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_reciever] += _amount;
+        emit Transfer(msg.sender, _reciever, _amount);
+        return true;
+    }
 }
